8a485075e1b1a064f3526dd9847cef22a3ee0d74
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 19:56:31 -0400

    Update views.py

--- ntnbms/views.py
+++ ntnbms/views.py
@@ -101,40 +101,24 @@ def view(request, key):
 
 
 
-
-
-
-
-   # def edit_record(request, key): 
-   # detail = personal_information.objects.get(InternID=key)
-   # result = internship_history.objects.filter(InternID=key)
-   # query_set = qualifications_on_entry.objects.filter(InternID=key)
-   # return render(request, 'ntnbms/edit.html', {'personal_information': detail, 'internship_history': result, 'qualifications_on_entry': query_set})
+def edit_record(request, key): 
+    detail = booking.objects.get(BID=key)
+    return render(request, 'ntnbms/edit.html', {'booking': detail})
 
 
 
 
 def update_record(request, key):
-    det = personal_information.objects.get(InternID=key)
-    res = internship_history.objects.filter(InternID=key)
-    sett = qualifications_on_entry.objects.filter(InternID=key)
+    det = booking.objects.get(CID=key)
     if request.method == 'POST':
-        det.First_Name = request.POST.get('two')  
-        det.Last_Name = request.POST.get('four') 
-        det.Other_Name = request.POST.get('six')  
-        #det.Date_of_Birth = request.POST.get('ten') 
-        det.Gender = request.POST.get('eight')
-        det.Nationality = request.POST.get('twelve')
-        det.Email = request.POST.get('fourteen')
-        det.Home_Telephone = request.POST.get('one')
-        det.Mobile_Telephone = request.POST.get('three')
-        det.Permanent_Address_Line1 = request.POST.get('five')
-        det.Permanent_Address_Line2 = request.POST.get('seven')
-        det.City = request.POST.get('nine')
-        det.Country = request.POST.get('eleven')
-        det.Current_Address_Line1 = request.POST.get('thirteen')
-        det.Current_Address_Line2 = request.POST.get('fifteen')
-        det.Documents = request.POST.get('sixteen')
+        det.Recepient_Name = request.POST.get('three')  
+        det.Number_Of_Showing = request.POST.get('four') 
+        det.Air_Time = request.POST.get('five')   
+        det.Text_Content = request.POST.get('six')
+        det.Air_Date = request.POST.get('seven')
+        det.Image_File = request.POST.get('eight')
+        det.Video_File = request.POST.get('nine')
+        det.Date_Created = request.POST.get('ten')
         det.save()
         return HttpResponseRedirect('/ntnbms/edit_record/%s/' % key)
 
