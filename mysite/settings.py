530e2fa1957ad2a105836a54699c159ad84ea709
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 10:00:21 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+#to handle media files -automatically provided by django
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
