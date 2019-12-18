32fda49d37f46d61e18554247a8c127da3969b46
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 02:23:01 -0400

    Update views.py

--- ntnbms/views.py
+++ ntnbms/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
 def login(request):
