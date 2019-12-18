ad0c5ab3aeb8ad06854c2d16a4935c45c5bdbd13
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 02:15:02 -0400

    Update views.py

--- ntnbms/views.py
+++ ntnbms/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'ntnbms/login.html', {})
+
+
+def home(request):
+    return render(request, 'ntnbms/home.html', {})
