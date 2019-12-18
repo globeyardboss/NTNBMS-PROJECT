a0fa947a041bd4f4824d9fb87120d4b15dde1509
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 02:31:46 -0400

    Update views.py

--- ntnbms/views.py
+++ ntnbms/views.py
@@ -14,3 +14,11 @@ def login(request):
 
 def home(request):
     return render(request, 'ntnbms/home.html', {})
+
+
+
+def view(request, key):
+    detail = personal_information.objects.get(InternID=key)
+    result = internship_history.objects.filter(InternID=key)
+    query_set = qualifications_on_entry.objects.filter(InternID=key)
+    return render(request, 'database/view.html', {'personal_information': detail, 'internship_history': result, 'qualifications_on_entry': query_set})
