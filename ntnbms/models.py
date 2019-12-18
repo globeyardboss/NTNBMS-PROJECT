bc1547bb4d4703ba800990eac1f08a568f3b1e08
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 10:00:00 -0400

    Update models.py

--- ntnbms/models.py
+++ ntnbms/models.py
@@ -16,3 +16,23 @@ class customer(models.Model):
 
     def __str__(self):
         return self.Last_Name
+
+
+
+
+
+class booking(models.Model):
+    BID = models.IntegerField(primary_key=True, null=False)
+    CID = models.ForeignKey('customer', on_delete=models.CASCADE)
+    Recipient_Name = models.CharField(max_length=25) 
+    Air_Time = models.DateTimeField(blank=True, null=True)
+    Number_Of_Showing = models.IntegerField()
+    Text_Content = models.TextField(blank=True)
+    Image_File = models.ImageField(default='default.png', blank=True)
+    Video_File = models.FileField(upload_to='uploads/', null=True, blank=True)
+    Air_Date = models.DateField(null=True, blank=True)
+    Date_Created = models.DateField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.Recipient_Name
