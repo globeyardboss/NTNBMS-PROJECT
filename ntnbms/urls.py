6ee13090ea852abafac13009bee36545fd258150
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 02:22:57 -0400

    Update urls.py

--- ntnbms/urls.py
+++ ntnbms/urls.py
@@ -1,5 +1,19 @@
+from django.conf.urls import url
+from django.contrib import admin
 from django.urls import path
 from . import views
+
+
+
+
+urlpatterns = [
+
+
+    url(r'^getdata/', views.new),
+
+
+]
+
 
 
 urlpatterns = [
