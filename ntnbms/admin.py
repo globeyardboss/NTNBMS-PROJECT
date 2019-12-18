e8497cc219366e46a383075e554438c4c9f76cde
stephie-lj <37296357+stephie-lj@users.noreply.github.com>
2019-12-18 15:56:55 -0400

    Ntn logo

--- ntnbms/admin.py
+++ ntnbms/admin.py
@@ -6,3 +6,6 @@ admin.site.register(customer)
 admin.site.register(booking)
 
 
+admin.site.site_title = "<NTNBMS>"
+admin.site.site_header = "NTN Booking and Management System Admin Page"
+admin.site.index_title = "Site Administration"
