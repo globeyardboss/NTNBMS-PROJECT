0a9c26cd49a4d4ae069fd00e4d18913245aeca35
wilsonzeenat <32527945+wilsonzeenat@users.noreply.github.com>
2019-12-18 21:11:18 -0400

    Update urls.py

--- ntnbms/urls.py
+++ ntnbms/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('ntnbms/search', views.search, name='search'),
     path('ntnbms/view/<key>/', views.view, name='view'), 
     path('ntnbms/edit_record/<key>/', views.edit_record, name='edit_record'),
+    path('ntnbms/edit_customer_record/<key>/', views.edit_customer_record, name='edit_customer_record'),
     path('ntnbms/update_record/<key>/', views.update_record, name='update_record'),
+    path('ntnbms/update_customer_record/<key>/', views.update_customer_record, name='update_customer_record'), 
     path('ntnbms/new', views.new, name='new'),
     path('ntnbms/newCustomer', views.newCustomer, name='newCustomer'),
 
